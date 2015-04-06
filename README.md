1762c0f881b667f9357ca7498ee3995c8847e01f
ilumos <ilumos@gmail.com>
2015-04-06 20:13:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+HTMLPurifier URI scheme validator for steam:// links for interacting with Valve's Steam application
+
+Set the `URI.AllowedSchemes` config variable to include `steam` and you will then be able to use `steam://` protocol links in your purified HTML.
---
+++ src/HTMLPurifier_URIScheme_steam.php
@@ -0,0 +1,30 @@
+<?php
+
+class HTMLPurifier_URIScheme_steam extends HTMLPurifier_URIScheme
+{
+    /**
+     * @type bool
+     */
+    public $browsable = false;
+
+    /**
+     * @type bool
+     */
+    public $may_omit_host = true;
+
+    /**
+     * @param HTMLPurifier_URI $uri
+     * @param HTMLPurifier_Config $config
+     * @param HTMLPurifier_Context $context
+     * @return bool
+     */
+    public function doValidate(&$uri, $config, $context)
+    {
+        $uri->userinfo = null;
+        $uri->port     = null;
+        $uri->query = null;
+        return true;
+    }
+}
+
+// vim: et sw=4 sts=4
